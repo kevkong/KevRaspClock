7c636b76514f0bd8c0e607071698af5b31d34633
kevkong <kknom1@gmail.com>
2023-12-13 18:38:38 +0800

    cpu-thermal -> cpu_thermal

--- sysstatus.py
+++ sysstatus.py
@@ -4,7 +4,7 @@ print(psutil.sensors_temperatures())
 
 class SYSSTATUS(object):
     def getCpuTemp(self):
-        temp = psutil.sensors_temperatures()['cpu-thermal'][0].current
+        temp = psutil.sensors_temperatures()['cpu_thermal'][0].current
         return str(round(temp,1)) + " " + chr(176) + "C"
     
     def getCpuLoad(self):
